a35e12999be19d7252d2be216ce7d0949c8a38a9
Moshood Kareem <42604147+Moshk94@users.noreply.github.com>
2020-07-13 17:47:23 +0100

    Refactored main code
    - Added doc block to the full JS file
    - Changed the way each node is stored
    - Added docblock
    - Algorithim will now output just path co-ordinates rather than f,g and h scores, id and parent id

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 My own implementation on the A star pathfinding. When the algorithm cannot find a path - the algorithim will start breaking walls to get to the target location.
 
 ___
-#### Inputs - World, Start Node,  End node
+#### Inputs - World, Start Node, End Node
 
  - World must be a nested array - 0 indicates valid tiles
- - Start/End node - must be a 1x2 array where the first value is the Y co-rodinates and the second value is the X co-ordinates. (0,0) is on the top left of the world.
+ - Start/End node - must be a 2x1 array where the first value is the Y co-rodinates and the second value is the X co-ordinates - e.g [y,x]. (0,0) is on the top left of the world.
 
 #### Output
-An array with co-ordinates to the end path. Each co-ordinate is an object with the **f**,**g** and **h** score, **x** and **y** co-ordinates, **id** and **parent id**
+A nested array with co-ordinates to the end path. The first value is the Y co-rodinates and the second value is the X co-ordinates - e.g [y,x].
 ___
-**Minfied size - 1.68kb** | **Full size - 6.12kb**
+**Minfied size - 1.53kb** | **Full size - 5.26kb**
 
 ___
 References: [https://www.raywenderlich.com/3016-introduction-to-a-pathfinding](https://www.raywenderlich.com/3016-introduction-to-a-pathfinding)
--- pathfind.js
+++ pathfind.js
@@ -1,175 +1,126 @@
 "use strict"
-function pathFind(worldMap, startPoint, endPoint){
+/**
+ * [0,0] is on the top left of the clonedworld. For the start and end array first value is the Y co-rodinates and the second value is the X co-ordinates;
+ * @param {array} clonedworld Nested clonedworld array - 0 Indicates valid tiles. 
+ * @param {array} start 2x1 array of Co-ordinates.
+ * @param {array} end 2x1 array of Co-ordinates.
+ * @return {array} Nested Array with co-ordinates to the end path
+ */
+function pathFind(world, start, end){
     const clone = (items) => items.map(item => Array.isArray(item) ? clone(item) : item);
     const randomInt = (max) => Math.floor(Math.random() * Math.floor(max));
-
-    var finalList = [];
-    var openList = [];
-    var closedList = [];
-    var wallList = [];
-    var world = clone(worldMap);
+    const g = (x,y) => Math.abs(start[0]-y)+Math.abs(start[1]-x)
+    const h = (x,y) => Math.abs(end[0]-y)+Math.abs(end[1]-x)
     
-    var currentNode = {
-        y: startPoint[0],
-        x: startPoint[1],
-        g: g(startPoint[0],startPoint[1]),
-        id: `${startPoint[0]}${startPoint[1]}`
+    let finalList = [];
+    let openList = [];
+    let closedList = [];
+    let wallList = [];
+    let clonedworld = clone(world);
+    let currentNode,startNode,finalNode;
+    
+    class nodes {
+        constructor(x,y,p) {
+            this.id = `${y}${x}`;
+            this.x = x;
+            this.y = y;
+            this.g = g(x,y);
+            this.h = h(x,y);
+            this.f = g(x,y) + h(x,y);
+            this.parent = p;
+        };
     };
-
-    var finalNode = {
-        id: `${endPoint[0]}${endPoint[1]}`
-    };
-
-    var startNode = {
-        id: `${startPoint[0]}${startPoint[1]}`
-    };
-
+    
+    currentNode = startNode = new nodes(start[1],start[0])
+    finalNode = new nodes(end[1],end[0])
     closedList.push(currentNode);
+
     var runTime = 0;
 
     while(currentNode.id != finalNode.id){
+        if(runTime >= (clonedworld.length * clonedworld[0].length)){
+            var randomWallIndex = randomInt(wallList.length);
+            var breakY = wallList[randomWallIndex].y;
+            var breakX = wallList[randomWallIndex].x;
+            clonedworld[breakY][breakX] = 0;
+            openList.push(wallList[randomWallIndex]);
+        };
 
-    if(runTime >= (world.length * world[0].length)){
-        var randomWallIndex = randomInt(wallList.length);
-        var breakY = wallList[randomWallIndex].y;
-        var breakX = wallList[randomWallIndex].x;
-        world[breakY][breakX] = 0;
-        openList.push(wallList[randomWallIndex]);
-    };
-            
-    if((currentNode.y - 1 >= 0)){
-        if(!(closedList.some(obj => obj.id == `${currentNode.y - 1}${currentNode.x}`))){
-            var northG = currentNode.g + 1;
-            var northH = h(currentNode.y - 1,currentNode.x);
-            var northF = northG + northH;
-            var NNodeProperty = {
-                y: currentNode.y - 1,
-                x: currentNode.x,
-                g: northG,
-                h: northH,
-                f: northF,
-                parent: `${currentNode.y}${currentNode.x}`,
-                id: `${currentNode.y - 1}${currentNode.x}`
-            }
-            if((world[currentNode.y - 1][currentNode.x] == 0)){
-                openList.push(NNodeProperty);
-            }else if((world[currentNode.y - 1][currentNode.x] == 1)){
-                if(!(wallList.some(obj => obj.id == `${currentNode.y - 1}${currentNode.x}`))){
-                    wallList.push(NNodeProperty);
-                };
-            };           
-        };   
-    };
+        let parent = `${currentNode.y}${currentNode.x}`;
 
-    if((currentNode.y + 1 < world.length)){
-        if(!(closedList.some(obj => obj.id == `${currentNode.y + 1}${currentNode.x}`))){
-            var southG = currentNode.g + 1;
-            var southH = h(currentNode.y + 1,currentNode.x);
-            var southF = southG + southH;
-            var SNodeProperty = {
-                y: currentNode.y + 1,
-                x: currentNode.x,
-                g: southG,
-                h: southH,
-                f: southF,
-                parent: `${currentNode.y}${currentNode.x}`,
-                id: `${currentNode.y + 1}${currentNode.x}`
+        if((currentNode.y - 1 >= 0)){
+            if(!(closedList.some(obj => obj.id == `${currentNode.y - 1}${currentNode.x}`))){
+                let newNode = new nodes(currentNode.x,currentNode.y-1,parent);
+                
+                if((clonedworld[currentNode.y - 1][currentNode.x] == 0)){
+                    openList.push(newNode);
+                }else if((clonedworld[currentNode.y - 1][currentNode.x] == 1)){
+                    if(!(wallList.some(obj => obj.id == `${currentNode.y - 1}${currentNode.x}`))){
+                        wallList.push(newNode);
+                    };
+                };   
             };
-            if((world[currentNode.y + 1][currentNode.x] == 0)){
-                openList.push(SNodeProperty);
-            }else if((world[currentNode.y + 1][currentNode.x] == 1)){
-                if(!(wallList.some(obj => obj.id == `${currentNode.y + 1}${currentNode.x}`))){
-                    wallList.push(SNodeProperty);
-                };
-            };
-        };  
-    };
+        };
 
-    if((currentNode.x + 1 < world[0].length)){
-        if(!(closedList.some(obj => obj.id == `${currentNode.y}${currentNode.x + 1}`))){
-            var eastG = currentNode.g + 1;
-            var eastH = h(currentNode.y,currentNode.x + 1);
-            var eastF = eastG + eastH
-            var ENodeProperty = {
-                y: currentNode.y,
-                x: currentNode.x + 1,
-                g: eastG,
-                h: eastH,
-                f: eastF,
-                parent: `${currentNode.y}${currentNode.x}`,
-                id: `${currentNode.y}${currentNode.x + 1}`
-            };
-
-            if((world[currentNode.y][currentNode.x + 1] == 0)){
-                openList.push(ENodeProperty)
-            }else if((world[currentNode.y][currentNode.x + 1] == 1)){
-                if(!(wallList.some(obj => obj.id == `${currentNode.y}${currentNode.x + 1}`))){
-                    wallList.push(ENodeProperty);
+        if((currentNode.y + 1 < clonedworld.length)){
+            if(!(closedList.some(obj => obj.id == `${currentNode.y + 1}${currentNode.x}`))){
+                let newNode = new nodes(currentNode.x,currentNode.y+1,parent);
+                if((clonedworld[currentNode.y + 1][currentNode.x] == 0)){
+                    openList.push(newNode);
+                }else if((clonedworld[currentNode.y + 1][currentNode.x] == 1)){
+                    if(!(wallList.some(obj => obj.id == `${currentNode.y + 1}${currentNode.x}`))){
+                        wallList.push(newNode);
+                    };
                 };
             };
         };
-    };
 
-    if((currentNode.x - 1 >= 0)){
-        if(!(closedList.some(obj => obj.id == `${currentNode.y}${currentNode.x - 1}`))){
-            var westG = currentNode.g + 1;
-            var westH = h(currentNode.y,currentNode.x - 1);
-            var westF = westG + westH;
-            var WNodeProperty = {
-                y: currentNode.y,
-                x: currentNode.x - 1,
-                g: westG,
-                h: westH,
-                f: westF,
-                parent: `${currentNode.y}${currentNode.x}`,
-                id: `${currentNode.y}${currentNode.x - 1}`
-            };
-
-            if((world[currentNode.y][currentNode.x - 1] == 0)){
-                openList.push(WNodeProperty);
-            }else if((world[currentNode.y][currentNode.x - 1] == 1)){
-                if(!(wallList.some(obj => obj.id == `${currentNode.y}${currentNode.x - 1}`))){
-                    wallList.push(WNodeProperty);
+        if((currentNode.x + 1 < clonedworld[0].length)){
+            if(!(closedList.some(obj => obj.id == `${currentNode.y}${currentNode.x + 1}`))){
+                let newNode = new nodes(currentNode.x + 1,currentNode.y,parent);
+                if((clonedworld[currentNode.y][currentNode.x + 1] == 0)){
+                    openList.push(newNode)
+                }else if((clonedworld[currentNode.y][currentNode.x + 1] == 1)){
+                    if(!(wallList.some(obj => obj.id == `${currentNode.y}${currentNode.x + 1}`))){
+                        wallList.push(newNode);
+                    };
                 };
             };
-        };     
-    };
-
-    runTime++
-    var lowestF = 0;
-    var arrayLoc = 0;
-    for(var i = 0; i < openList.length; i++){
-        if(i == 0){lowestF = openList[i].f};
-        if(openList[i].f < lowestF){
-            lowestF = openList[i].f
-            arrayLoc = i;
         };
-        if(i == openList.length-1){
-            currentNode = openList[arrayLoc]
-            closedList.push(openList[arrayLoc]);
-            runTime = 0;
-            openList.splice(arrayLoc,1);
-        };
-    };
 
+        if((currentNode.x - 1 >= 0)){
+            if(!(closedList.some(obj => obj.id == `${currentNode.y}${currentNode.x - 1}`))){
+                let newNode = new nodes(currentNode.x - 1,currentNode.y,parent);
+                if((clonedworld[currentNode.y][currentNode.x - 1] == 0)){
+                    openList.push(newNode);
+                }else if((clonedworld[currentNode.y][currentNode.x - 1] == 1)){
+                    if(!(wallList.some(obj => obj.id == `${currentNode.y}${currentNode.x - 1}`))){
+                        wallList.push(newNode);
+                    };
+                };
+            };
+        };
+
+        if(openList.length != 0){
+            currentNode = openList[lowestFIndex(openList)]
+            closedList.push(openList[lowestFIndex(openList)]);
+            openList.splice(lowestFIndex(openList),1);
+        }else(runTime++)  
     };
 
     var pos = closedList.findIndex(x => x.id == finalNode.id);
     var referenceNode = closedList[pos];
-    finalList.unshift(referenceNode)
+    finalList.unshift([referenceNode.y,referenceNode.x])
     while(referenceNode.id != startNode.id){
         pos = closedList.findIndex(x => x.id == referenceNode.parent);
         referenceNode = closedList[pos];
-        finalList.unshift(referenceNode)
+        finalList.unshift([referenceNode.y,referenceNode.x])
     };
-
-    function h(nodeY, nodeX){
-        return (Math.abs(endPoint[0]-nodeY)+Math.abs(endPoint[1]-nodeX));
-    };
-
-    function g(nodeY, nodeX){
-        return (Math.abs(startPoint[0]-nodeY)+Math.abs(startPoint[1]-nodeX));
-    };
-        
     return finalList;
+
+    function lowestFIndex(a) {
+        let lowestVal = a.reduce(function(res, obj) {return (obj.f < res.f) ? obj : res});
+        let pos = a.map(function(e) { return e.f; }).indexOf(lowestVal.f);
+        return pos;
+      };
 };
